ad9b5c7fe1d3b4fc3b5c3ba3c97c424df8a51e57
LorMeBioAI <103401277+LorMeBioAI@users.noreply.github.com>
2026-07-24 13:34:46 +0800

    Delete train_dec.py

--- train_dec.py
+++
@@ -1,6 +0,0 @@
-from vae_dec import dec_util
-if __name__=="__main__":
-
-    dec_util.train_dec('./raw/SRR13128012.fastq','./SRR13128012', 4,[128,128],5,30000)
-
-    #dec_util.test_dec( 4,[128,128],10,'cuda')
